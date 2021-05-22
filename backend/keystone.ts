7672d308483742ce5d9505719ffe112026defab2
Mahendra <34649305+MMahendra001@users.noreply.github.com>
2021-05-22 15:51:10 +0530

    update:mongoDB Atlas,keystone,cloudinary,Seed Data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -5,6 +5,7 @@ import { withItemData, statelessSessions } from '@keystone-next/keystone/session
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -35,8 +36,13 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add Data Seeding Here.
-    },
+      async onConnect(keystone) {
+        console.log('Connected to a Database.');
+        if(process.argv.includes('--seed-data')){
+          await insertSeedData(keystone);
+        }
+      },
+     },
     lists: createSchema({
       // Schema items go in here
       User,
