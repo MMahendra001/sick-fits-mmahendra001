3e02dd933548e0bdfb372417f9685322d981e527
Mahendra <34649305+MMahendra001@users.noreply.github.com>
2021-06-07 12:39:26 +0530

    update:user reg. and auth:SignIn

---
+++ frontend/components/SignIn.js
@@ -0,0 +1,81 @@
+import { gql, useMutation } from '@apollo/client';
+import DisplayError from './ErrorMessage';
+import useForm from '../lib/useForm';
+import Form from './styles/Form';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    authenticateUserWithPassword(email: $email, password: $password) {
+      ... on UserAuthenticationWithPasswordSuccess {
+        item {
+          id
+          name
+          email
+        }
+      }
+      ... on UserAuthenticationWithPasswordFailure {
+        code
+        message
+      }
+    }
+  }
+`;
+
+export default function SignIn() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+    variables: inputs,
+    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(inputs);
+    const res = await signin();
+    console.log(res);
+    resetForm();
+  }
+
+  const error =
+    data?.authenticateUserWithPassword.__typename ===
+    'UserAuthenticationWithPasswordFailure'
+      ? data?.authenticateUserWithPassword
+      : undefined;
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>SignIn Into Your Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email Address"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your Password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
+    </Form>
+  );
+}
