6d44bdc84e471c2fae1bac230bdd4c51d2e753b6
Mahendra <34649305+MMahendra001@users.noreply.github.com>
2021-07-10 22:40:12 +0530

    update:permission access functions : yes | no

--- backend/access.ts
+++ backend/access.ts
@@ -1,5 +1,28 @@
+import { permissionList } from "./schemas/fields";
 import { ListAccessArgs } from "./types";
 
 export function isSignedIn({ session }: ListAccessArgs) {
     return !!session;
 }
+
+
+const generatedPermissions = Object.fromEntries(
+    permissionList.map((permission) => [
+        permission,
+        function ({ session }: ListAccessArgs) {
+            return !!session?.data.role?.[permission];
+        },
+    ])
+);
+
+
+// Permission check if someone needs a criteria - yes or no.
+
+export const permission = {
+    ...generatedPermissions,
+    isAwesome({ session }: ListAccessArgs) {
+        return session.data.name.includes('mahi');
+    }
+}
+
+//TODO: Rule based functions
